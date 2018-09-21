d1de5e13160c25264f3aa51550030fd4fd9410e9
John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
2018-09-21 22:31:55 +0100

    Fix FTBFS on m68k
    ===================================================================
    
    Gbp-Pq: Name fix-ftbfs-m68k.patch

--- Source/WebCore/dom/NodeRareData.cpp
+++ Source/WebCore/dom/NodeRareData.cpp
@@ -38,6 +38,10 @@ struct SameSizeAsNodeRareData {
     void* m_pointer[3];
 };
 
+#if defined(__m68k__)
+COMPILE_ASSERT(sizeof(NodeRareData) <= sizeof(SameSizeAsNodeRareData), NodeRareDataShouldStaySmall);
+#else
 COMPILE_ASSERT(sizeof(NodeRareData) == sizeof(SameSizeAsNodeRareData), NodeRareDataShouldStaySmall);
+#endif
 
 } // namespace WebCore
