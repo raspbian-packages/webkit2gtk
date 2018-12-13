26617a3d4833061e6d47d67608cfcaaa8537fa9a
John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
2018-12-13 18:43:29 +0000

    Fix FTBFS on m68k
    ===================================================================
    
    Gbp-Pq: Name fix-ftbfs-m68k.patch

--- Source/WebCore/platform/graphics/FontDescription.cpp
+++ Source/WebCore/platform/graphics/FontDescription.cpp
@@ -52,7 +52,11 @@ struct SameSizeAsFontCascadeDescription {
     unsigned bitfields3 : 10;
 };
 
+#if defined(__m68k__)
+COMPILE_ASSERT(sizeof(FontCascadeDescription) <= sizeof(SameSizeAsFontCascadeDescription), FontCascadeDescription_should_stay_small);
+#else
 COMPILE_ASSERT(sizeof(FontCascadeDescription) == sizeof(SameSizeAsFontCascadeDescription), FontCascadeDescription_should_stay_small);
+#endif
 
 FontDescription::FontDescription()
     : m_fontSelectionRequest { FontCascadeDescription::initialWeight(), FontCascadeDescription::initialStretch(), FontCascadeDescription::initialItalic() }
