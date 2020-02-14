a6618dc8d14f877cddf249af41f3d3caec5428e8
John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
2020-02-14 13:55:40 +0000

    Fix FTBFS on m68k
    Bug-Debian: https://bugs.debian.org/868126
    Last-Update: 2020-01-23
    
    ===================================================================
    
    Gbp-Pq: Name fix-ftbfs-m68k.patch

--- Source/WebCore/rendering/style/RenderStyle.cpp
+++ Source/WebCore/rendering/style/RenderStyle.cpp
@@ -66,7 +66,11 @@ struct SameSizeAsBorderValue {
     int m_restBits;
 };
 
+#if defined(__m68k__)
+COMPILE_ASSERT(sizeof(BorderValue) <= sizeof(SameSizeAsBorderValue), BorderValue_should_not_grow);
+#else
 COMPILE_ASSERT(sizeof(BorderValue) == sizeof(SameSizeAsBorderValue), BorderValue_should_not_grow);
+#endif
 
 struct SameSizeAsRenderStyle {
     void* dataRefs[7];
@@ -84,7 +88,11 @@ struct SameSizeAsRenderStyle {
 #endif
 };
 
+#if defined(__m68k__)
+static_assert(sizeof(RenderStyle) <= sizeof(SameSizeAsRenderStyle), "RenderStyle should stay small");
+#else
 static_assert(sizeof(RenderStyle) == sizeof(SameSizeAsRenderStyle), "RenderStyle should stay small");
+#endif
 
 RenderStyle& RenderStyle::defaultStyle()
 {
