8c8332db698e07edb53d051c3821f04b75ef9522
Alberto Garcia <berto@igalia.com>
2021-03-18 15:05:45 +0000

    Reduce memory usage when not using the Gold linker
    Bug-Debian: https://bugs.debian.org/949621
    Forwarded: no
    
    ===================================================================
    
    Gbp-Pq: Name reduce-memory-overheads.patch

--- Source/cmake/OptionsCommon.cmake
+++ Source/cmake/OptionsCommon.cmake
@@ -95,6 +95,12 @@ option(GCC_OFFLINEASM_SOURCE_MAP
   "Produce debug line information for offlineasm-generated code"
   ${GCC_OFFLINEASM_SOURCE_MAP_DEFAULT})
 
+# Pass --reduce-memory-overheads to the bfd linker in order to save memory
+if (NOT USE_LD_GOLD)
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--reduce-memory-overheads")
+    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--reduce-memory-overheads")
+endif ()
+
 # Enable the usage of OpenMP.
 #  - At this moment, OpenMP is only used as an alternative implementation
 #    to native threads for the parallelization of the SVG filters.
