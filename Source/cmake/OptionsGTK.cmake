6fb26ac71619e28dfeab2a5386e30ea5e7eb0a42
Alberto Garcia <berto@igalia.com>
2022-11-04 16:37:25 +0000

    Enable THREADS_PREFER_PTHREAD_FLAG
    Bug: https://bugs.webkit.org/show_bug.cgi?id=182622
    Bug-Debian: https://bugs.debian.org/895969
    Origin: https://trac.webkit.org/changeset/231843
    
    This fixes a FTBFS in riscv64
    ===================================================================
    
    Gbp-Pq: Name prefer-pthread.patch

--- Source/cmake/OptionsGTK.cmake
+++ Source/cmake/OptionsGTK.cmake
@@ -13,6 +13,8 @@ endif ()
 
 set(USER_AGENT_BRANDING "" CACHE STRING "Branding to add to user agent string")
 
+set(THREADS_PREFER_PTHREAD_FLAG ON)
+
 find_package(Cairo 1.14.0 REQUIRED)
 find_package(Fontconfig 2.8.0 REQUIRED)
 find_package(Freetype 2.4.2 REQUIRED)
